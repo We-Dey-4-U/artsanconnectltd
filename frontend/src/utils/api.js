ba1154503c6e00ac814fbeac870860b9d8c1414f
highbridge <github@highbridgehomesltd.com>
2025-09-01 22:50:52 -0700

    Build frontend and update dependencies

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api/auth'; // Adjust the base URL for your backend. adjjust
+const API_BASE_URL = 'http://localhost:5000/api/auth'; // Adjust the base URL for your backend. adjjust install icon
 
 export const registerUser = async (userData) => {
   const response = await fetch(`${API_BASE_URL}/register`, {
